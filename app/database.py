9ffdd57dfa5d5f1f38e21135569b601cf112a816
ksee1230 <ksee1230@gmail.com>
2025-06-24 13:17:41 +0900

    fix: DB URI 롤백

--- app/database.py
+++ app/database.py
@@ -12,18 +12,18 @@ hosts = os.getenv('MONGO_HOSTS')
 db_name = os.getenv('MONGO_DB')
 replica_set = os.getenv('MONGO_REPLICA_SET')
 
-# MONGO_URI = (
-#     f"mongodb://{user}:{password}@{hosts}/{db_name}?authSource=admin&replicaSet={replica_set}"
-# )
-
-# 로컬에서 테스트할 때 쓸 코드
-# 위의 코드 안 먹히면 그냥 아래 꺼 주석 풀고 쓸 생각
-single_host = "localhost:27017" # 테스트하려는 노드의 로컬 주소
-
 MONGO_URI = (
-    f"mongodb://{user}:{password}@{single_host}/{db_name}"
-    f"?authSource=admin&directConnection=true"
+    f"mongodb://{user}:{password}@{hosts}/{db_name}?authSource=admin&replicaSet={replica_set}"
 )
+
+# # 로컬에서 테스트할 때 쓸 코드
+# # 위의 코드 안 먹히면 그냥 아래 꺼 주석 풀고 쓸 생각
+# single_host = "localhost:27017" # 테스트하려는 노드의 로컬 주소
+
+# MONGO_URI = (
+#     f"mongodb://{user}:{password}@{single_host}/{db_name}"
+#     f"?authSource=admin&directConnection=true"
+# )
 
 client = AsyncIOMotorClient(MONGO_URI)
 
